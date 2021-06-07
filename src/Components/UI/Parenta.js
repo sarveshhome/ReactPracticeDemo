a85b997fcb99662fc675ec02c52e0dc7a7505cea
sarveshhome <sarvesh.carlton@gmail.com>
2021-06-07 19:31:07 +0100

    build

--- src/Components/UI/Parenta.js
+++ src/Components/UI/Parenta.js
@@ -3,7 +3,15 @@ import Childa from './Childa';
 const Parenta =()=>{
 
     return (        
-            <div><Childa name="Tarun" age="20"/> </div>        
+            <div>
+                <Childa name="Tarun" age="20"/> 
+               
+                <script src="https://gist.github.com/sarveshhome/5ba7eed676cec5a4da7c793316225605.js">
+                    
+                </script>
+            </div>   
+            
+            
     );
 }
 
