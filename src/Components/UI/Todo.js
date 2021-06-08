8cc5f1acf4bc80e102cc41db3452d83611370218
sarveshhome <sarvesh.carlton@gmail.com>
2021-06-08 19:13:27 +0100

    todo add

--- src/Components/UI/Todo.js
+++ src/Components/UI/Todo.js
@@ -1,19 +1,64 @@
+import { useState } from 'react';
 import styles from './Todo.module.css';
 
-const todoHandler =(event)=>{
-    event.preventDefault();
-   console.log('form submit');
-}
+
 
 const Todo =()=>{
-    return (        
-            <form onSubmit={todoHandler}>
-                <div className={styles.todo}>
-                    <h1>ToDo</h1>
-                    <input name="todoName" type="text"/>
-                    <input type="submit" value="Submit"/>
+    const [todofinaldata,setTodofinaldata] = useState();
+    const [todovalue,setTodoValue] = useState('');
+    const [todoid, setTodoid]= useState(0);    
+    let itemToRender;
+
+    const todoEnterHandler =(event)=>{
+        setTodoValue(event.target.value);
+    }
+
+    const todoHandler =(event)=>{
+        event.preventDefault();
+        setTodofinaldata( 
+                {id:todoid,
+                todovalue:todovalue}
+            
+        );
+        //setTodoValue();
+        setTodoid(todoid +1);       
+       
+    }
+
+   
+   
+    if(todofinaldata){
+        console.log(todofinaldata);      
+    }
+    else {
+       
+      }
+
+    return ( 
+        <div className="container">
+            <div className="row">
+             <div className="buttonsubmit">
+                <form onSubmit={todoHandler}>
+                    <div className={styles.todo}>
+                        <h1>ToDo</h1>
+                        <input name="todoName" onChange={todoEnterHandler} type="text"/>
+                        <input type="submit" value="Submit"/>
+                    </div>
+                </form>  
+            </div> 
+            </div>       
+            <div className="row">
+                <div className="todolist">
+                    <ul>
+                    if(todofinaldata){
+                    itemToRender = todofinaldata.map(data =>{
+                         <li key={data.id}> data.todovalue</li>
+                     })
+                   }
+                </ul>
                 </div>
-            </form>        
+            </div>
+        </div>
     );
 }
 
